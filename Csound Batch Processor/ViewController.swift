b7e60664403192f53aecbe5b9c49c4fdfd7105ad
ryanlaney <ryanlaney@gmail.com>
2015-10-07 20:57:10 -0500

    Initial Commit

---
+++ Csound Batch Processor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Csound Batch Processor
+//
+//  Created by Ryan Laney on 10/7/15.
+//  Copyright © 2015 Ryan Laney. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
